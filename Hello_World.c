62885499d19a0c4557b8e69e56d2b75cd67fbea4
MeetMali <96412583+MeetMali@users.noreply.github.com>
2021-12-20 13:46:49 +0530

    Initial commit

---
+++ Hello_World.c
@@ -0,0 +1,12 @@
+//AIM: WAP to print 'Hello World'.
+
+#include <stdio.h>
+
+int main()
+{
+    printf("________________\n");
+    printf("\nHello World.\n");
+    printf("________________\n");
+    return 0;
+}
+
